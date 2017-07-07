08d26eaeca6a563a6cf671cf9ed84206cc8f728d
bit <bit@DESKTOP-KP396JD>
2017-07-07 18:58:31 +0900

    8완료

--- src/com/javaex/problem08/BookShop.java
+++ src/com/javaex/problem08/BookShop.java
@@ -27,7 +27,7 @@ public class BookShop {
         sc.close();
 
         // (1) 입력된 번호에 맞는 책을 찾아 대여 되었음(상태코드=0)을 체크 합니다.
-        books[num-1].rent();
+        books[num].rent();
 
         System.out.println("*****도서 정보 출력하기******");
         displayBookInfo(books);
